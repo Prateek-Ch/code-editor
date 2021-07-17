b5bba5b59e5130000cc2f740187427e55a4c071a
Prateek Chaturvedi <prateekchaturvedi216@gmail.com>
2021-07-17 21:40:43 +0530

    Modified Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,7 +10,7 @@ function Header(){
             <div className={classes.logo}><i class="fas fa-code"></i> Code to React</div>
             <nav>
                 <ul>
-                    <li><NavLink exact to='/' activeClassName={classes.html_active}><i class="fab fa-html5"></i> Html </NavLink></li> 
+                    <li><NavLink exact to='/' activeClassName={classes.html_active}><i class="fab fa-html5"></i> HTML </NavLink></li> 
                     <li><NavLink exact to='/css' activeClassName={classes.css_active}><i class="fab fa-css3"></i> Css </NavLink></li>
                     <li><NavLink exact to='/js' activeClassName={classes.js_active}><i class="fab fa-js"></i> Js </NavLink></li>
                 </ul>
